15ab53d654aac5967cb8b9b31f592de5c13e3c3e
Trishu_guru_ <strishu82@gmail.com>
2026-06-16 15:44:14 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let arr = [23,"23","trishu","sharma","ujjain"];
+arr.pop(2);
+console.log(arr);
+
+
+let arr1 = [20,"12","trishu","ujjain","sharma"];
+arr1.pop();
+console.log(arr1);
+
+let arr = [23,"23","trishu","sharma","ujjain"];
+arr.push("guru");
+console.log(arr);
+
+let arr = [23,"23","trishu","sharma","ujjain"];
+arr.splice("23");
+console.log(arr);
+
+
