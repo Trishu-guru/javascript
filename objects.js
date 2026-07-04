687456f16adab31fc13ebf717177b0854f700784
Trishu_guru_ <strishu82@gmail.com>
2026-07-04 20:46:30 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,16 @@
+// Object using object literal
+let person = {
+  name: "Alice",
+  age: 25,
+  city: "New York"
+};
+
+// Object using constructor function
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+let person2 = new Person("Bob", 30);
+
+
